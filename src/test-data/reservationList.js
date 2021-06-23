75135eb498b4e8afa9d3c5ff283c00c4e2765e62
emmtqg <elizabeth.m.mcmahon@gmail.com>
2021-06-22 22:34:49 -0400

    Added test data file for reservations

---
+++ src/test-data/reservationList.js
@@ -0,0 +1,44 @@
+const reservationList = [
+  {
+    id: "d4297a67-cfde-4841-81a8-aa90f719255b",
+    start: "2021-02-03T15:30:00.000Z",
+    end: "2021-02-03T15:45:00.000Z",
+    room: {
+      id: "401662b9-b110-4159-a4f2-ebd955f3b1f4",
+      name: "Room A",
+      imageUrl: "./imgs/yibei-geng--UdYbiywGeg-unsplash.jpg",
+    },
+  },
+  {
+    id: "306df302-d117-4257-80df-856bba2cd644",
+    start: "2021-02-03T15:35:00.000Z",
+    end: "2021-02-03T15:50:00.000Z",
+    room: {
+      id: "70866847-3a55-407e-9973-841ac4c16a29",
+      name: "Room B",
+      imageUrl: "./imgs/damir-kopezhanov-VM1Voswbs0A-unsplash.jpg",
+    },
+  },
+  {
+    id: "49c8819b-d22c-4d32-9fa2-7979dd14528c",
+    start: "2021-02-03T15:50:00.000Z",
+    end: "2021-02-03T16:50:00.000Z",
+    room: {
+      id: "401662b9-b110-4159-a4f2-ebd955f3b1f4",
+      name: "Room A",
+      imageUrl: "./imgs/yibei-geng--UdYbiywGeg-unsplash.jpg",
+    },
+  },
+  {
+    id: "072bfec3-f0d9-4cf7-b6b6-6a2c9301fe87",
+    start: "2020-08-01T03:30:00.000Z",
+    end: "2020-08-01T05:00:00.000Z",
+    room: {
+      id: "70866847-3a55-407e-9973-841ac4c16a29",
+      name: "Room B",
+      imageUrl: "./imgs/damir-kopezhanov-VM1Voswbs0A-unsplash.jpg",
+    },
+  },
+];
+
+export default reservationList;
